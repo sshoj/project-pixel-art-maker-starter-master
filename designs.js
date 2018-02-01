1e23eec988b318b2ae765b74c66ba9e33f1345b5
sshoj <33101981+sshoj@users.noreply.github.com>
2018-02-01 14:08:39 -0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,7 +1,7 @@
 
 /* Udacity " Grow with Google Challenge Scholarship: Front-End Web Dev" project/
-Code written by "Seyyed Saeed Hojabrossadati"*/
-
+@Author "Seyyed Saeed Hojabrossadati"*/
+Var PixCan=$('#pixelCanvas');
 //Event listener for calling makeGride function on clicking button.
 $('#button').on("click",function makeGrid() {
   //reset the table for next enter
@@ -11,13 +11,13 @@ $('#button').on("click",function makeGrid() {
   let inHeight=$('#inputHeight').val();
   let inWidth=$('#inputWidth').val();
   for (let row=0;row<inHeight;row++){
-    $('#pixelCanvas').append('<tr></tr>')};
+   PixCan.append('<tr></tr>')};
   for (let col=0;col<inWidth;col++){
     $('tr').append('<td></td>')};
  })
 
  //assinging color value on table's event listener
-  $('#pixelCanvas').on('click', "td", function(clk) {
+ PixCan.on('click', "td", function(clk) {
     let colors=$('#colorPicker').val();
     if (clk.which === 1) {
       $(this).css("background", colors);
